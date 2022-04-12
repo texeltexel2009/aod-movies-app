ff593e0991292cc003cd5ec0487cad5bc271bed7
texeltexel2009 <texeltexel2009@gmail.com>
2022-04-12 13:10:25 +0400

    Add Head component to layout
    ... and set title + font

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>AOD Movies</title>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
