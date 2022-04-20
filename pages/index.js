5ed82b0124f0caaba05c35710c743fcf170240c9
texeltexel2009 <texeltexel2009@gmail.com>
2022-04-20 18:26:51 +0400

    Add React Pagination

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,77 @@
+import React from 'react'
+import { Router, withRouter } from 'next/router'
+import ReactPaginate from 'react-paginate'
 import tmdb from '../configs/tmdb'
-import Moviescontainer from '../components/CardContainer'
+import PostsContainer from '../components/PostsContainer'
 
-export default function Home({ moviesData }) {
-  const { page, results, total_pages, total_results } = moviesData;
-  //console.log(page)
-  //console.log(total_pages)
-  //console.log(total_results)
+const Home = ( props ) => {
+  // When new page selected in paggination, we take current path and query parrams.
+  // Then add or modify page parram and then navigate to the new route.
+  const paginationHandler = ( page ) => {
+    const currentPath = props.router.pathname
+    const currentQuery = props.router.query
+    currentQuery.page = page.selected + 1
+
+    props.router.push( {
+      pathname: currentPath,
+      query: currentQuery,
+    } )
+  }
+
+  let content
+  if ( props.isAnimating ) {
+    content = (
+      <span className="visually-hidden">Loading...</span>
+    )
+  } else {
+    content = (
+      <PostsContainer data={props.posts} type="movies" />
+    )
+  }
+
 
   return (
-    <Moviescontainer data={results} type="movies" />
+    <>
+      {content}
+
+      <ReactPaginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        activeClassName={'active'}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        initialPage={props.currentPage - 1}
+        pageCount={props.pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={paginationHandler}
+      />
+    </>
   )
 }
 
-export const getStaticProps = async () => {
-  const movies = await tmdb.discover( 'movie', [
+Home.getInitialProps = async ({ query }) => {
+  const page = query.page || 1
+  const tmdbData = await tmdb.discover( 'movie', [
     {
       param: 'sort_by',
       value: 'vote_count.desc',
     },
     {
       param: 'page',
-      value: 1
+      value: page
     },
   ] )
 
   return {
-    props: {
-      moviesData: movies,
-    },
+    totalCount: tmdbData.total_results,
+    pageCount: tmdbData.total_pages,
+    currentPage: tmdbData.page,
+    perPage: 20,
+    posts: tmdbData.results,
   }
 }
+
+export default withRouter( Home )
