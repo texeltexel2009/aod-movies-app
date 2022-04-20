1b2da602c08d921ae58a7bde1299b48d7644306d
texeltexel2009 <texeltexel2009@gmail.com>
2022-04-20 09:48:19 +0400

    Add top header loading progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,37 @@
-import Layout from '../components/layout'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Layout from '../components/Layout'
+import { Progress } from '../components/Progress'
+import { useProgressStore } from '../store'
 import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp ( { Component, pageProps } ) {
+  const setIsAnimating = useProgressStore( ( state ) => state.setIsAnimating )
+  const isAnimating = useProgressStore( ( state ) => state.isAnimating )
+  const router = useRouter()
+  useEffect( () => {
+    const handleStart = () => {
+      setIsAnimating( true )
+    }
+    const handleStop = () => {
+      setIsAnimating( false )
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [ router ] )
+
   return (
-    <ThemeProvider
-      attribute="class"
-      storageKey="nightwind-mode"
-      defaultTheme="system"
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Progress isAnimating={isAnimating} />
+      <Component {...pageProps} />
+    </Layout>
   )
 }
