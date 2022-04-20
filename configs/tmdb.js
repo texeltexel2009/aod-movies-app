db58f15f2a55fe018da924e94ad20fb44c6f8556
texeltexel2009 <texeltexel2009@gmail.com>
2022-04-20 18:25:23 +0400

    Fix tmdb api key not being available to client

--- configs/tmdb.js
+++ configs/tmdb.js
@@ -1,5 +1,5 @@
 import Moviestmdb from 'moviestmdb'
 
-const tmdb = new Moviestmdb( process.env.TMDB_API_KEY )
+const tmdb = new Moviestmdb( process.env.NEXT_PUBLIC_TMDB_API_KEY )
 
 export default tmdb
