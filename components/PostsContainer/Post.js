851063000047d818a3013055788c0347436e39b1
texeltexel2009 <texeltexel2009@gmail.com>
2022-04-22 17:36:46 +0400

    Improves Posts Container and Cards styling

--- components/PostsContainer/Post.js
+++ components/PostsContainer/Post.js
@@ -11,33 +11,33 @@ export default function Post(props) {
       <Link href={`/${type}/${genSlug(title || name, id)}`}>
         <a className="flex w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto cursor-pointer rounded-xl transform shadow-lg hover:shadow-2xl text-white">
           <Image className="relative block w-full object-cover object-top group-hover:scale-110 transition duration-300 ease-in-out" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} layout='fill' />
-          <div className="absolute inset-0 z-10 transition duration-300 ease-in-out bg-gradient-to-t from-[#000000cc] via-[#0000009e] to-transparent"></div>
-          <div className="w-full absolute bottom-0 group z-10 p-10 space-y-6">
+          <div className="absolute inset-0 z-1 transition duration-300 ease-in-out bg-gradient-to-t from-[#000000cc] via-[#0000009e] to-transparent"></div>
+          <div className="w-full absolute bottom-0 group z-1 p-6 md:p-10 space-y-3 md:space-y-6">
             <div className="align-self-end w-full">
-              <div className="space-y-6">
+              <div className=" space-y-3 md:space-y-6">
                 <div className="flex flex-col space-y-2 inner">
-                  <h1 className="text-2xl font-bold text-white" data-unsp-sanitized="clean">{title || name}</h1>
+                  <h1 className="text-lg md:text-2xl font-bold text-white" data-unsp-sanitized="clean">{title || name}</h1>
                 </div>
-                <div className="flex flex-row justify-between datos">
+                <div className="hidden md:flex flex-row justify-between text-xs md:text-sm text-xs md:text-sm">
                   <div className="flex flex-col">
                     <div className="popularity">{popularity}</div>
-                    <div className="text-sm text-gray-400">Popularity:</div>
+                    <div className="text-gray-400">Popularity:</div>
                   </div>
                   <div className="flex flex-col">
                     <div className="release">{release_date}</div>
-                    <div className="text-sm text-gray-400">Release date:</div>
+                    <div className="text-gray-400">Release date:</div>
                   </div>
                   <div className="flex flex-col">
                     <div className="runtime">{vote_average}</div>
-                    <div className="text-sm text-gray-400">Vote Average:</div>
+                    <div className="text-gray-400">Vote Average:</div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <button type="button" className="absolute top-3 right-3 z-20 flex-shrink-0 rounded-full ml-3 p-1 border-2 border-white text-white hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500">
+          <button type="button" className="absolute top-3 right-3 z-2 flex-shrink-0 rounded-full ml-3 p-1 border-2 border-white text-white hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500">
             <span className="sr-only">Add to favorites</span>
-            <HeartIcon className="h-6 w-6" aria-hidden="true" />
+            <HeartIcon className="h-4 w-4 md:h-6 md:w-6" aria-hidden="true" />
           </button>
         </a>
       </Link>
--- components/PostsContainer/index.js
+++ components/PostsContainer/index.js
@@ -2,7 +2,7 @@ import Post from './Post'
 
 export default function PostsContainer({ data, type }) {
   return (
-    <div className="px-4 py-4 sm:px-0 gap-5 grid grid-flow-row grid-cols-4">
+    <div className="px-4 py-4 pb-8 sm:px-0 gap-5 grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data.map( ( d ) => (
         <Post key={d.id} type={type} {...d} />
       ) )}
